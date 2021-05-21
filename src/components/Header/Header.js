2b1c64ae4ac8293750c76998105f9b25e8ccdaa6
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2021-05-21 12:53:38 +0200

    Mobile header with improvements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
 import UnstyledButton from '../UnstyledButton';
+import VisuallyHidden from '../VisuallyHidden';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -17,45 +18,44 @@ const Header = () => {
   // <button onClick={() => setShowMobileMenu(true)}>
 
   return (
-    <HeaderWrapper>
+    <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
+        </LogoWrapper>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-          <UnstyledButton>
+        </DesktopNav>
+        <MobileActions>
+          <ShoppingBagButton>
             <Icon id="shopping-bag" strokeWidth={2} />
-          </UnstyledButton>
+            <VisuallyHidden>Open cart</VisuallyHidden>
+          </ShoppingBagButton>
           <UnstyledButton>
             <Icon id="search" strokeWidth={2} />
+            <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
           <UnstyledButton onClick={() => setShowMobileMenu(true)}>
             <Icon id="menu" strokeWidth={2} />
+            <VisuallyHidden>Navigation menu</VisuallyHidden>
           </UnstyledButton>
-        </Nav>
-        <Side />
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu
         isOpen={showMobileMenu}
         onDismiss={() => setShowMobileMenu(false)}
       />
-    </HeaderWrapper>
+    </header>
   );
 };
-
-const HeaderWrapper = styled.header`
-  @media ${QUERIES.tabletAndDown} {
-    border-top: 4px solid ${COLORS.gray[900]};
-  }
-`;
 
 const MainHeader = styled.div`
   display: flex;
@@ -65,31 +65,57 @@ const MainHeader = styled.div`
   border-bottom: 1px solid ${COLORS.gray[300]};
 
   @media ${QUERIES.tabletAndDown} {
+    border-top: 4px solid ${COLORS.gray[900]};
+    justify-content: space-between;
     align-items: center;
+  }
+
+  @media ${QUERIES.phoneAndDown} {
+    padding-left: 16px;
+    padding-right: 16px;
   }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
-  gap: clamp(
-    1rem,
-    7vw - 1.75rem,
-    2rem
-  );
+  gap: 48px;
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndDown} {
-
-    margin-right: 0;
-    margin-left: auto;
+    display: none;
   }
 `;
 
-const Side = styled.div`
+const MobileActions = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: flex;
+    gap: 32px;
+  }
+
+  @media ${QUERIES.phoneAndDown} {
+    gap: 16px;
+  }
+`;
+
+const ShoppingBagButton = styled(UnstyledButton)`
+  transform: translateX(-2px);
+`;
+
+const LogoWrapper = styled.div`
   flex: 1;
 
   @media ${QUERIES.tabletAndDown} {
-    flex: 0;
+    flex: revert;
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
   }
 `;
 
@@ -102,10 +128,6 @@ const NavLink = styled.a`
 
   &:first-of-type {
     color: ${COLORS.secondary};
-  }
-
-  @media ${QUERIES.tabletAndDown} {
-    display: none;
   }
 `;
 
